60ee8bb04021b747f3b7b34e38b9bd110fbafa56
hasherr <eklein217@gmail.com>
2016-07-18 21:46:39 -0400

    started on javascript

--- program.js
+++ program.js
@@ -1,3 +1,27 @@
+// var a=0;
+// function count() {
+//     a++;
+//     document.getElementById('do-count').innerHTML = a;
+// }
+// document.getElementById('do-count').onclick = count;
+
+var activeTag = document.getElementById("active_tag");
+var activateButton = document.getElementById("activate");
+
+function toggle()
+{
+	if (activeTag.innerHTML == "Kill-Switch is NOT active") {
+		activeTag.innerHTML = "Kill-Switch is active";
+		activeTag.style.color = "green";
+	}
+	else {
+		activeTag.innerHTML = "Kill-Switch is NOT active";
+		activeTag.style.color = "red";
+	}
+}
+
+activateButton.onclick = toggle;
+
 function getAllTabURLs() {
 	var urls = [];
 	index = 0;
@@ -13,10 +37,11 @@ function getAllTabURLs() {
 	return urls;
 }
 
-function killUnproductiveTabs() {
-	var allUrls = getAllTabURLs();
+// ERROR SOMEWHERE HERE!!!!!!!!!
+// function killUnproductiveTabs() {
+// 	var allUrls = getAllTabURLs();
 
-	for (int i = 0; i < allUrls.length - 1; i++) {
-
-	}
-}
+// 	for (int i = 0; i < allUrls.length - 1; i++) {
+// 		for (int i = 0; i < )
+// 	}
+// }
