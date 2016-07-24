5e75fea250cc8e92531a77c8ac3ec45c7d6e563d
Evan <eklein217@gmail.com>
2016-07-23 23:47:19 -0400

    added fonts, finished UI

--- program.js
+++ program.js
@@ -1,25 +1,4 @@
-// var a=0;
-// function count() {
-//     a++;
-//     document.getElementById('do-count').innerHTML = a;
-// }
-// document.getElementById('do-count').onclick = count;
-var activeTag = document.getElementById("active_tag");
 var activateButton = document.getElementById("activate");
-
-function toggle() {
-	if (activeTag.innerHTML == "Kill-Switch is NOT active") {
-		activeTag.innerHTML = "Kill-Switch is active";
-		activeTag.style.color = "green";
-		removeUnproductiveTabs();
-	}
-	else {
-		activeTag.innerHTML = "Kill-Switch is NOT active";
-		activeTag.style.color = "red";
-	}
-}
-
-activateButton.onclick = toggle;
 
 function removeUnproductiveTabs() {
 	var list = ["imgur.com", "youtube.com", "facebook.com", "reddit.com"];
@@ -27,25 +6,11 @@ function removeUnproductiveTabs() {
 		for (i = 0; i < allTabs.length; i++) {
 			for (j = 0; j < list.length; j++) {
 				if (allTabs[i].url.indexOf(list[j]) != -1) {
-					chrome.tabs.remove(allTabs[i].id, function() {});
+					chrome.tabs.remove(allTabs[i].id, function(){});
 				}
 			}
 		}
 	});
 }
 
-// function killUnproductiveTabs() {
-// 	var allUrls = getAllTabURLs();
-// 	alert(allUrls[1]);
-// 	var reader = new FileReader();
-//
-// 	var blacklist = reader.readAsText("blacklist.json");
-// 	for (int i = 0; i < allUrls.length; i++) {
-// 		for (int j = 0; j < blacklist.blacklist.length, j++) {
-// 			if (allUrls[i].contains(blacklist.blacklist[j]))
-// 			{
-// 				chrome.tabs.remove(i, function());
-// 			}
-// 		}
-// 	}
-// }
+activateButton.onclick = removeUnproductiveTabs;
