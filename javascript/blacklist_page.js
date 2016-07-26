361428c25c8f45436643b0e93bba2b752683a45d
Evan <eklein217@gmail.com>
2016-07-26 18:13:21 -0400

    production shipped!

--- javascript/blacklist_page.js
+++ javascript/blacklist_page.js
@@ -1,15 +1,61 @@
 var addSiteField = document.getElementById('web_entry');
 var updateButton = document.getElementById('update_blacklist');
 
+function checkForFirstOpening() {
+  // chrome.storage.local.get('first', function(data) {
+  //   if (data == null) {
+  //     chrome.storage.local.set({'urls':[]}, function() {
+  //       data = 'Not null';
+  //       chrome.storage.local.set({'first':data});
+  //     });
+  //   }
+  // });
+}
+
 function generateRows() {
-  var list = new Array();
-  var table = document.getElementById('site_list');
-  chrome.storage.local.get('urls', function(data) {
-    list = data.urls;
-    for (i = 0; i < list.length; i++) {
-      table.innerHTML += '<tr><th>' + list[i] + '</th><th><input type="checkbox" name="' + list[i] + '"></th></tr>';
+  chrome.storage.local.get('first', function(data) {
+    var isEmpty = true;
+    for(var prop in data) {
+      if(data.hasOwnProperty(prop)) {
+        isEmpty = false;
+      }
+    }
+    if (isEmpty) {
+      console.log('please');
+      chrome.storage.local.set({'urls':["https://www.facebook.com/", "https://www.youtube.com/", "https://twitter.com/"]}, function() {
+        data = 'Not null';
+        chrome.storage.local.set({'first':data});
+
+        var list = new Array();
+        var table = document.getElementById('site_list');
+        chrome.storage.local.get('urls', function(data) {
+          list = data.urls;
+          for (i = 0; i < list.length; i++) {
+            table.innerHTML += '<tr><th>' + list[i] + '</th><th><input type="checkbox" name="' + list[i] + '"></th></tr>';
+          }
+        });
+      });
+    }
+    else {
+      var list = new Array();
+      var table = document.getElementById('site_list');
+      chrome.storage.local.get('urls', function(data) {
+        list = data.urls;
+        for (i = 0; i < list.length; i++) {
+          table.innerHTML += '<tr><th>' + list[i] + '</th><th><input type="checkbox" name="' + list[i] + '"></th></tr>';
+        }
+      });
     }
   });
+  // checkForFirstOpening();
+  // var list = new Array();
+  // var table = document.getElementById('site_list');
+  // chrome.storage.local.get('urls', function(data) {
+  //   list = data.urls;
+  //   for (i = 0; i < list.length; i++) {
+  //     table.innerHTML += '<tr><th>' + list[i] + '</th><th><input type="checkbox" name="' + list[i] + '"></th></tr>';
+  //   }
+  // });
 }
 
 function updateBlacklist() {
